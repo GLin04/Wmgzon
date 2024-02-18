4416be80db867af2a946ed2d399ff7150f4a300d
u5500299 <gordon.lin@warwick.ac.uk>
2024-02-18 19:43:33 +0000

    feat: added html form for register input and corrected route location to auth

--- website/auth.py
+++ website/auth.py
@@ -2,3 +2,6 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/register')
+def login():
+    return render_template("register.html")
--- website/views.py
+++ website/views.py
@@ -11,9 +11,6 @@ def home():
 def login():
     return render_template("login.html")
 
-@views.route('/register')
-def register():
-    return render_template("register.html")
 
 @views.route('/coming_soon')
 def coming_soon():
