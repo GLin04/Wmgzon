41d59dd2957d6e37bd289cf9c8b9e0264b7920b5
Gordon Lin <g.lin02@hotmail.com>
2024-02-20 18:11:27 +0000

    feat: added table for basket and altered table for professtional installation option in products

--- website/models.py
+++ website/models.py
@@ -15,9 +15,16 @@ cursor.execute('''
         deliveryTime INTEGER NOT NULL,
         brand TEXT NOT NULL,
         specifications TEXT NOT NULL,
-        description TEXT NOT NULL
+        description TEXT NOT NULL,
+        professional_installation BOOLEAN NOT NULL DEFAULT FALSE
     )
 ''')
+
+# In case i need to edit a table
+# cursor.execute('''
+#     ALTER TABLE products
+#     ADD COLUMN professional_installation BOOLEAN NOT NULL DEFAULT FALSE
+#                ''')
 
 # Create the register form table
 cursor.execute('''
@@ -26,6 +33,16 @@ cursor.execute('''
         name TEXT NOT NULL,
         password TEXT NOT NULL,
         admin BOOLEAN NOT NULL DEFAULT FALSE
+    )
+''')
+
+# Create the basket table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS basket (
+        email TEXT NOT NULL PRIMARY KEY,               
+        productid INTEGER NOT NULL,
+        product_quantity INTEGER NOT NULL,
+        professional_installation BOOLEAN NOT NULL DEFAULT FALSE
     )
 ''')
 
