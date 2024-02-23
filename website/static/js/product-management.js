938e7c8fb1799f27af8917166de3a4409228649b
Gordon Lin <g.lin02@hotmail.com>
2024-02-23 14:01:48 +0000

    feat: added totoal price that updates when quantity is changed

--- website/static/js/product-management.js
+++ website/static/js/product-management.js
@@ -10,3 +10,13 @@
 function redirectElectronics() {
     window.location.href = '/electronics';
 }
+
+function calculateTotal() {
+    var quantity = document.getElementById('quantity').value;
+    var total = quantity * price;
+    document.getElementById('total').innerHTML = 'TOTAL = £' + total.toFixed(2);
+}
+
+function warn() {
+    alert('This product is currently out of stock');
+}
