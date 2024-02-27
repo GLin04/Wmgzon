f5c57d6820f8b3f28c3624dd4a9de4e71b5026ae
Gordon Lin <g.lin02@hotmail.com>
2024-02-27 18:33:26 +0000

    feat: users can now filter by product type and/or price with or without the search function

--- website/electronics.py
+++ website/electronics.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, session
 import sqlite3
 electronics = Blueprint('electronics', __name__)
 
-@electronics.route('/')
+@electronics.route('/' , methods=['GET', 'POST'])
 def products():
 
 
@@ -12,11 +12,46 @@ def products():
     cursor.execute("SELECT * FROM products")
     products = cursor.fetchall()
 
-    # close the connection
-    conn.close()
+    if request.method == 'GET':
+        conn.close()
+        return render_template("electronics.html", products=products)
+    
+    elif request.method == 'POST':
+        filter_list = []
+        filter_list += request.form.getlist('accessories_filter')
+        filter_list += request.form.getlist('data_storage_filter')
+        filter_list += request.form.getlist('desktop_filter')
+        filter_list += request.form.getlist('laptop_filter')
+        filter_list += request.form.getlist('monitor_filter')
+        filter_list += request.form.getlist('phone_filter')
+        filter_list += request.form.getlist('printer_filter')
+        filter_list += request.form.getlist('tv_filter')
+        max_price = request.form['max_price']
 
-    # render the template with the products list
-    return render_template("electronics.html", products=products)
+        if len(filter_list) == 0:
+            cursor.execute('SELECT * FROM products')
+            filter_only_products = cursor.fetchall()
+        else:
+            get_filter_products = '''
+                SELECT * FROM products
+                WHERE productType IN ({})
+            '''.format(','.join(['?'] * len(filter_list)))
+
+            cursor.execute(get_filter_products, filter_list)
+            filter_only_products = cursor.fetchall()
+        
+        cursor.execute('''SELECT * FROM products
+                    WHERE price <= ?''', (max_price,))
+
+        max_price_products = cursor.fetchall()
+
+        filtered_products = [product for product in max_price_products if product in filter_only_products]
+
+
+
+        conn.close()
+        return render_template("electronics.html", products=filtered_products , filter_list=filter_list)
+    
 
 @electronics.route('/product/<int:product_id>')
 def product(product_id):
@@ -40,21 +75,75 @@ def product(product_id):
 
 
 
-@electronics.route('/search', methods=['POST'])
+@electronics.route('/search', methods=['POST', 'GET'])
 def search():
-    if request.method == 'POST':
-        keyword = request.form['search_input']
+    conn = sqlite3.connect('wmgzon.db')
+    cursor = conn.cursor()
 
-        conn = sqlite3.connect('wmgzon.db')
-        cursor = conn.cursor()
+    try:
+        session['search_input'] = request.form['search_input']
+        search_input = session['search_input']
+    except:
+        session['search_input'] = ''
+        search_input = session['search_input']
+
+
+    if request.method == 'GET':
+        
+        cursor.execute('SELECT * FROM products')
+        filtered_products = cursor.fetchall()
+        conn.close()
+        return render_template('search_results.html', search_input=search_input, filtered_products=filtered_products)
+    
+    if request.method == 'POST':
+
+
+
+        filter_list = []
+        filter_list += request.form.getlist('accessories_filter')
+        filter_list += request.form.getlist('data_storage_filter')
+        filter_list += request.form.getlist('desktop_filter')
+        filter_list += request.form.getlist('laptop_filter')
+        filter_list += request.form.getlist('monitor_filter')
+        filter_list += request.form.getlist('phone_filter')
+        filter_list += request.form.getlist('printer_filter')
+        filter_list += request.form.getlist('tv_filter')
+
+        try:
+            max_price = request.form['max_price']
+        except:
+            max_price = 1000000
 
         cursor.execute('''
             SELECT * FROM products
             WHERE name LIKE ?
-        ''', ('%' + keyword + '%',))
+        ''', ('%' + search_input + '%',))
 
-        results = cursor.fetchall()
+        searched_products = cursor.fetchall()
+
+        if len(filter_list) == 0:
+            cursor.execute('SELECT * FROM products')
+            filter_only_products = cursor.fetchall()
+        else:
+            get_filter_products = '''
+                SELECT * FROM products
+                WHERE productType IN ({})
+            '''.format(','.join(['?'] * len(filter_list)))
+
+            cursor.execute(get_filter_products, filter_list)
+            filter_only_products = cursor.fetchall()
+
+        cursor.execute('''SELECT * FROM products
+                    WHERE price <= ?''', (max_price,))
+
+        max_price_products = cursor.fetchall()
+
+        
+        searched_filtered_products = [product for product in searched_products if product in filter_only_products]
+
+        filtered_products = [product for product in searched_filtered_products if product in max_price_products]
+
+
+
         conn.close()
-
-        # Pass the results to the search_results template
-        return render_template('search_results.html', results=results, search_term=keyword)
+        return render_template('search_results.html', filtered_products=filtered_products, search_input=search_input, filter_list=filter_list)
